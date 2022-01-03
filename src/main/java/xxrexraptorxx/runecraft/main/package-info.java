4338d5db0a73f38a13592777e5fcfd80e9369772
XxRexRaptorxX <Raptor-Simon@web.de>
2022-01-03 02:37:19 +0100

    added wands to jeed 'recipes', added missing jei description

--- src/main/java/xxrexraptorxx/runecraft/main/package-info.java
+++ src/main/java/xxrexraptorxx/runecraft/main/package-info.java
@@ -3,3 +3,5 @@
  * @projectPage https://www.curseforge.com/minecraft/mc-mods/rune-craft
  **/
 package xxrexraptorxx.runecraft.main;
+
+// added missing information to a jei description, added wands to jeed
