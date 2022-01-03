4f1952c9f6bf9f2d10b5f0089206b8cc3816a09a
XxRexRaptorxX <Raptor-Simon@web.de>
2022-01-03 02:18:17 +0100

    package-info

--- src/main/java/xxrexraptorxx/runecraft/main/RuneCraft.java
+++ src/main/java/xxrexraptorxx/runecraft/main/RuneCraft.java
@@ -1,17 +1,17 @@
 package xxrexraptorxx.runecraft.main;
 
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import xxrexraptorxx.runecraft.utils.Config;
 import xxrexraptorxx.runecraft.world.OreGenerator;
 
+/**
+ * @author XxRexRaptorxX (RexRaptor)
+ * @projectPage https://www.curseforge.com/minecraft/mc-mods/rune-craft
+ **/
 @Mod(References.MODID)
 public class RuneCraft {
 
--- src/main/java/xxrexraptorxx/runecraft/main/package-info.java
+++ src/main/java/xxrexraptorxx/runecraft/main/package-info.java
@@ -1,6 +1,5 @@
-package xxrexraptorxx.runecraft.main;
-
 /**
  * @author XxRexRaptorxX (RexRaptor)
  * @projectPage https://www.curseforge.com/minecraft/mc-mods/rune-craft
  **/
+package xxrexraptorxx.runecraft.main;
