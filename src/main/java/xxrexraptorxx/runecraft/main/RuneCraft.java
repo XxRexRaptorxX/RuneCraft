ffa756197914b73d600620823dd5793a2ae1a127
XxRexRaptorxX <Raptor-Simon@web.de>
2022-03-18 21:01:09 +0100

    clean up

--- src/main/java/xxrexraptorxx/runecraft/main/RuneCraft.java
+++ src/main/java/xxrexraptorxx/runecraft/main/RuneCraft.java
@@ -1,15 +1,12 @@
 package xxrexraptorxx.runecraft.main;
 
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import xxrexraptorxx.runecraft.utils.Config;
 import xxrexraptorxx.runecraft.utils.ModSetup;
-import xxrexraptorxx.runecraft.world.OreGenerator;
 
 /**
  * @author XxRexRaptorxX (RexRaptor)
--- src/main/java/xxrexraptorxx/runecraft/main/package-info.java
+++ src/main/java/xxrexraptorxx/runecraft/main/package-info.java
@@ -3,5 +3,3 @@
  * @projectPage https://www.curseforge.com/minecraft/mc-mods/rune-craft
  **/
 package xxrexraptorxx.runecraft.main;
-
-// added missing information to a jei description, added wands to jeed
