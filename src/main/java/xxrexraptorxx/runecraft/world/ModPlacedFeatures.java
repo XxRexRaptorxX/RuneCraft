dfe42c98534fd9960be7fe1bd5e01858b734b015
XxRexRaptorxX <Raptor-Simon@web.de>
2022-09-02 19:58:30 +0200

    unused import removed

--- src/main/java/xxrexraptorxx/runecraft/world/ModPlacedFeatures.java
+++ src/main/java/xxrexraptorxx/runecraft/world/ModPlacedFeatures.java
@@ -7,7 +7,6 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 import xxrexraptorxx.runecraft.main.References;
-import xxrexraptorxx.runecraft.utils.Config;
 
 import java.util.List;
 
