4e2990523b8c33bb7d17d710c663d6d42ae349ff
XxRexRaptorxX <Raptor-Simon@web.de>
2025-10-20 21:55:04 +0200

    refactor: code improvements

--- src/main/java/xxrexraptorxx/runecraft/integration/JEIIntegration.java
+++ src/main/java/xxrexraptorxx/runecraft/integration/JEIIntegration.java
@@ -7,6 +7,7 @@ import mezz.jei.api.registration.IRecipeRegistration;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
+import xxrexraptorxx.magmacore.utils.FormattingHelper;
 import xxrexraptorxx.runecraft.main.References;
 import xxrexraptorxx.runecraft.registry.ModBlocks;
 import xxrexraptorxx.runecraft.registry.ModItems;
@@ -28,56 +29,56 @@ public class JEIIntegration implements IModPlugin {
     public void registerRecipes(IRecipeRegistration registry) {
         if (Config.getActivateRunestoneJeiDescription())
             registry.addIngredientInfo(new ItemStack(ModBlocks.RUNE_STONE.get()), VanillaTypes.ITEM_STACK,
-                    Component.translatable("message." + References.MODID + ".rune_stone_jei_desc"));
+                    FormattingHelper.setMessageComponent(References.MODID, "rune_stone_jei_desc"));
 
         registry.addIngredientInfo(AltarHelper.getAltarTreasureItems(), VanillaTypes.ITEM_STACK,
-                Component.translatable("message." + References.MODID + ".treasure_ritual_drops_jei_desc"));
+                FormattingHelper.setMessageComponent(References.MODID, "treasure_ritual_drops_jei_desc"));
 
         registry.addIngredientInfo(new ItemStack(ModItems.MAGICAL_BOOK.get()), VanillaTypes.ITEM_STACK,
-                Component.translatable("message." + References.MODID + ".magical_book_jei_desc"));
+                FormattingHelper.setMessageComponent(References.MODID, "magical_book_jei_desc"));
         registry.addIngredientInfo(new ItemStack(ModBlocks.RUIN_BLOCK.get()), VanillaTypes.ITEM_STACK,
-                Component.translatable("message." + References.MODID + ".ruin_block_jei_desc"));
-        registry.addIngredientInfo(new ItemStack(ModItems.ORB.get()), VanillaTypes.ITEM_STACK, Component.translatable("message." + References.MODID + ".orb_jei_desc"));
-        registry.addIngredientInfo(new ItemStack(ModItems.SOUL.get()), VanillaTypes.ITEM_STACK, Component.translatable("message." + References.MODID + ".soul_jei_desc"));
+                FormattingHelper.setMessageComponent(References.MODID, "ruin_block_jei_desc"));
+        registry.addIngredientInfo(new ItemStack(ModItems.ORB.get()), VanillaTypes.ITEM_STACK, FormattingHelper.setMessageComponent(References.MODID, "orb_jei_desc"));
+        registry.addIngredientInfo(new ItemStack(ModItems.SOUL.get()), VanillaTypes.ITEM_STACK, FormattingHelper.setMessageComponent(References.MODID, "soul_jei_desc"));
 
         registry.addIngredientInfo(new ItemStack(ModItems.SPELL_PAGE.get()), VanillaTypes.ITEM_STACK,
-                Component.translatable("message." + References.MODID + ".spell_page_jei_desc"));
+                FormattingHelper.setMessageComponent(References.MODID, "spell_page_jei_desc"));
         registry.addIngredientInfo(new ItemStack(ModItems.CURSE_PAGE.get()), VanillaTypes.ITEM_STACK,
-                Component.translatable("message." + References.MODID + ".curse_page_jei_desc"));
-        registry.addIngredientInfo(new ItemStack(ModItems.LOST_PAGE.get()), VanillaTypes.ITEM_STACK, Component.translatable("message." + References.MODID + ".lost_page_jei_desc"));
-        registry.addIngredientInfo(new ItemStack(ModItems.WAND_PAGE.get()), VanillaTypes.ITEM_STACK, Component.translatable("message." + References.MODID + ".wand_page_jei_desc"));
-        registry.addIngredientInfo(new ItemStack(ModItems.ORB_PAGE.get()), VanillaTypes.ITEM_STACK, Component.translatable("message." + References.MODID + ".orb_page_jei_desc"));
+                FormattingHelper.setMessageComponent(References.MODID, "curse_page_jei_desc"));
+        registry.addIngredientInfo(new ItemStack(ModItems.LOST_PAGE.get()), VanillaTypes.ITEM_STACK, FormattingHelper.setMessageComponent(References.MODID, "lost_page_jei_desc"));
+        registry.addIngredientInfo(new ItemStack(ModItems.WAND_PAGE.get()), VanillaTypes.ITEM_STACK, FormattingHelper.setMessageComponent(References.MODID, "wand_page_jei_desc"));
+        registry.addIngredientInfo(new ItemStack(ModItems.ORB_PAGE.get()), VanillaTypes.ITEM_STACK, FormattingHelper.setMessageComponent(References.MODID, "orb_page_jei_desc"));
         registry.addIngredientInfo(new ItemStack(ModItems.ENCHANTING_PAGE.get()), VanillaTypes.ITEM_STACK,
-                Component.translatable("message." + References.MODID + ".enchanting_page_jei_desc"));
+                FormattingHelper.setMessageComponent(References.MODID, "enchanting_page_jei_desc"));
         registry.addIngredientInfo(new ItemStack(ModItems.BANNED_PAGE.get()), VanillaTypes.ITEM_STACK,
-                Component.translatable("message." + References.MODID + ".banned_page_jei_desc"));
+                FormattingHelper.setMessageComponent(References.MODID, "banned_page_jei_desc"));
         registry.addIngredientInfo(new ItemStack(ModItems.ALCHEMY_PAGE.get()), VanillaTypes.ITEM_STACK,
-                Component.translatable("message." + References.MODID + ".alchemy_page_jei_desc"));
+                FormattingHelper.setMessageComponent(References.MODID, "alchemy_page_jei_desc"));
 
         registry.addIngredientInfo(new ItemStack(ModItems.AETHER_WAND.get()), VanillaTypes.ITEM_STACK,
-                Component.translatable("message." + References.MODID + ".aether_wand_jei_desc"));
+                FormattingHelper.setMessageComponent(References.MODID, "aether_wand_jei_desc"));
         registry.addIngredientInfo(new ItemStack(ModItems.NETHER_WAND.get()), VanillaTypes.ITEM_STACK,
-                Component.translatable("message." + References.MODID + ".nether_wand_jei_desc"));
+                FormattingHelper.setMessageComponent(References.MODID, "nether_wand_jei_desc"));
         registry.addIngredientInfo(new ItemStack(ModItems.STORM_WAND.get()), VanillaTypes.ITEM_STACK,
-                Component.translatable("message." + References.MODID + ".storm_wand_jei_desc"));
-        registry.addIngredientInfo(new ItemStack(ModItems.HOLY_WAND.get()), VanillaTypes.ITEM_STACK, Component.translatable("message." + References.MODID + ".holy_wand_jei_desc"));
+                FormattingHelper.setMessageComponent(References.MODID, "storm_wand_jei_desc"));
+        registry.addIngredientInfo(new ItemStack(ModItems.HOLY_WAND.get()), VanillaTypes.ITEM_STACK, FormattingHelper.setMessageComponent(References.MODID, "holy_wand_jei_desc"));
         registry.addIngredientInfo(new ItemStack(ModItems.CURSE_WAND.get()), VanillaTypes.ITEM_STACK,
-                Component.translatable("message." + References.MODID + ".curse_wand_jei_desc"));
+                FormattingHelper.setMessageComponent(References.MODID, "curse_wand_jei_desc"));
         registry.addIngredientInfo(new ItemStack(ModItems.MAELSTROM_WAND.get()), VanillaTypes.ITEM_STACK,
-                Component.translatable("message." + References.MODID + ".maelstrom_wand_jei_desc"));
+                FormattingHelper.setMessageComponent(References.MODID, "maelstrom_wand_jei_desc"));
         registry.addIngredientInfo(new ItemStack(ModItems.CREATURE_WAND.get()), VanillaTypes.ITEM_STACK,
-                Component.translatable("message." + References.MODID + ".creature_wand_jei_desc"));
+                FormattingHelper.setMessageComponent(References.MODID, "creature_wand_jei_desc"));
         registry.addIngredientInfo(new ItemStack(ModItems.DEFENSIVE_WAND.get()), VanillaTypes.ITEM_STACK,
-                Component.translatable("message." + References.MODID + ".defensive_wand_jei_desc"));
+                FormattingHelper.setMessageComponent(References.MODID, "defensive_wand_jei_desc"));
         registry.addIngredientInfo(new ItemStack(ModItems.DESTRUCTION_WAND.get()), VanillaTypes.ITEM_STACK,
-                Component.translatable("message." + References.MODID + ".destruction_wand_jei_desc"));
+                FormattingHelper.setMessageComponent(References.MODID, "destruction_wand_jei_desc"));
         registry.addIngredientInfo(new ItemStack(ModItems.CHANGING_WAND.get()), VanillaTypes.ITEM_STACK,
-                Component.translatable("message." + References.MODID + ".changing_wand_jei_desc"));
+                FormattingHelper.setMessageComponent(References.MODID, "changing_wand_jei_desc"));
         registry.addIngredientInfo(new ItemStack(ModItems.THUNDER_WAND.get()), VanillaTypes.ITEM_STACK,
-                Component.translatable("message." + References.MODID + ".thunder_wand_jei_desc"));
+                FormattingHelper.setMessageComponent(References.MODID, "thunder_wand_jei_desc"));
         registry.addIngredientInfo(new ItemStack(ModItems.ESCAPE_WAND.get()), VanillaTypes.ITEM_STACK,
-                Component.translatable("message." + References.MODID + ".escape_wand_jei_desc"));
+                FormattingHelper.setMessageComponent(References.MODID, "escape_wand_jei_desc"));
         registry.addIngredientInfo(new ItemStack(ModItems.BASIC_WAND.get()), VanillaTypes.ITEM_STACK,
-                Component.translatable("message." + References.MODID + ".basic_wand_jei_desc"));
+                FormattingHelper.setMessageComponent(References.MODID, "basic_wand_jei_desc"));
     }
 }
